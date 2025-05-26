83dd87ae6cbc4f5bdd594f20707f6d0aeba2f855
Nouhaila <nouhaila.rochdi@le-campus-numerique.fr>
2025-05-26 16:53:10 +0200

    en pprogression

--- square.py
+++ square.py
@@ -6,12 +6,20 @@ class Square:
     def __init__(self,x,y):
         self.x=x
         self.y=y
-        self.pawn=None                 # par défaut
+        self.pawn=None                # par défaut on considère que la case est vide
         list_adjacent=[]
-
-    def set_square(self,poins):              #si elle est vide on pose le poins 
+    
+    def empty_square(self):          # le cas si ma case est vide
+        if self.pawn is None:
+            return self.pawn
+        
+    def set_square(self,poins):              #pour poser : si elle est vide on pose le poins 
         self.poins=poins
         if self.empty_square():
-            return self.pawn=poins 
+            self.pawn=poins 
         else:
             return f"the square is already occuped"
+        
+    def __repr__(self):
+        return f"Square(Square={self.pawn})"
+    
