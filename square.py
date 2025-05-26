3e74482d1ecc26c194b021d706238efc1edb22f7
Nouhaila <nouhaila.rochdi@le-campus-numerique.fr>
2025-05-26 16:33:59 +0200

    Work in progress

---
+++ square.py
@@ -0,0 +1,19 @@
+from constants import BLACK, WHITE, BLACK_STR, WHITE_STR
+from pawn import Pawn
+
+class Square:
+
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+        self.pawn=None                 # par défaut
+        list_adjacent=[]
+
+    def set_square(self,poins):              #si elle est vide on pose le poins 
+        self.poins=poins
+        if self.empty_square():
+            return self.pawn=poins 
+        else:
+            return f"the square is already occuped"
+
+
