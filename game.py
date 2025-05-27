577d5774adc8b714008c8d62ae7678ac889dc74c
Yoann Julliard <yoann.julliard@le-campus-numerique.fr>
2025-05-27 10:21:56 +0200

    update game.py (not working because of not developped yet dependencies)

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-from board import Board
+# from board import Board
 from player import Player
 
 class Game:
@@ -8,17 +8,89 @@ class Game:
         self.player2 = Player("white")
         self.board = Board()
 
+    # NB: game_end, draw_board and MakeMove are not ready
     def run(self):
-        pass
+        while not self.game_end():
+            # first, draw the board
+            self.board.draw_board()
 
-    def str_to_coord(self):
-        pass
+            # then, start the player 1 turn 
+            print("---- Player 1 (black) turn ----")
+            # we ask player 1 to choose a move
+            player1_move_coord = self.str_to_coord(self.player1.play())
 
-    def flip_list(self):
-        pass
+            # while makemove is false (i.e., illegal move), asks again the player
+            while (not self.board.makeMove(self.player1.color, player1_move_coord)):
+                print('Invalid move. Try again!')
+                player1_move_coord = self.str_to_coord(self.player1.play())
 
-    def possible_move(self):
-        pass
+            # once the move is accepted and the changes on the board are done,
+            # we print the updated board
+            self.board.draw_board()
+
+            # then, start the player 2 turn
+            print("---- Player 2 (white) turn ----")
+            # we ask player 1 to choose a move
+            player2_move_coord = self.str_to_coord(self.player2.play())
+
+            # while makemove is false (i.e., illegal move), asks again the player
+            while (not self.board.makeMove(self.player1.color, player2_move_coord)):
+                print('Invalid move. Try again!')
+                player2_move_coord = self.str_to_coord(self.player2.play())
+
+            # end of the turn, we loop back (if the game_end is not true)
+        # if game_end is true, we print the scores
+        print("End of the game")
+        print("Player 1 (black) score: ", board.score[1])
+        print("Player 2 (white) score: ", board.score[0])
+
+    @staticmethod
+    def str_to_coord(move_str: str) -> tuple:
+        """Converts the move from an str format (user format) into a tuple format (program format)
+
+        Args:
+            move_str (str): user input
+
+        Returns:
+            tuple: column number, line number
+        """
+        move_letter = move_str[0:1]
+        move_int = int(move_str[1:2]) - 1
+        dict_convert = {
+            "A": 0,
+            "B": 1,
+            "C": 2,
+            "D": 3,
+            "E": 4,
+            "F": 5,
+            "G": 6,
+            "H": 7
+        }
+        return dict_convert[move_letter], move_int
+
+    # maybe this function should be in the Board class
+    # def flip_list(list_to_flip):
+    #     # for each element of the list, execute the flip method
+    #     for i in list_to_flip:
+    #         pawn.flip()
+    
+    def possible_moves(self):
+        # for each square of the board grid, check if there would be a legal
+        # move on this square and 
+        player1_possible_moves = None
+        player2_possible_moves = None
+        for square in board.grid:
+            if isinstance(board.legal_move(player1.color, square), list):
+                player1_possible_moves = board.legal_move(player1.color, square)
+            if isinstance(board.legal_move(player2.color, square), list):
+                player2_possible_moves = board.legal_move(player2.color, square)
+        return player1_possible_moves, player2_possible_moves
 
     def game_end(self):
-        pass
+        if self.possible_moves == (None, None):
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    print("str_to_coord: ", Game.str_to_coord("C4") == (2, 3))
