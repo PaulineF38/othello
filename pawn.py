131f6730a5d1dc372888fd6e5a2bfd16c61ce2e5
Nouhaila <nouhaila.rochdi@le-campus-numerique.fr>
2025-05-27 14:12:47 +0200

    square and pawn

--- pawn.py
+++ pawn.py
@@ -4,27 +4,50 @@ class Pawn:
 
     def __init__(self, color=BLACK):
         self.color=color
-    
+        """_summary_
+        init th color of the pawn
+        Raises:
+            ValueError: _description_
+
+        Returns:
+            _type_: _description_
+        """        
     @property
     def color(self):
+        """_summary_
+        acess getter color
+        Returns:
+            _type_: _description_
+        """        
         return self._color
     
     @color.setter
+    
     def color(self, color):
+        """_summary_
+        the getter of the attribut color
+        Args:
+            color (_type_): _description_
+
+        Raises:
+            ValueError: _description_
+        """        
         if color in [WHITE, BLACK]:
             self._color = color
         else:
-            raise ValueError ("color is not in the board")
+            raise ValueError ("color is not valid")
         
     def flip(self) -> None :
-        """Flip the Pawn"""
-
+        """_summary_
+        flip the pawn by the color
+        """          
         if self.color==WHITE:
             self.color=BLACK
         else:
             self.color=WHITE
     
     def __repr__(self):
+        
         return f"Pawn(color={self.color})"
     
     def __str__(self):
