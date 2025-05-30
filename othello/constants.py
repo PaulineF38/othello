b40d166e5359eba378ed868f598ed32fa4d7bf10
Xavier Bednarek <xavier.bednarek@le-campus-numerique.fr>
2025-05-30 16:41:21 +0200

    Improve constants. Constants should not be mutable : list --> tuple

--- othello/constants.py
+++ othello/constants.py
@@ -17,5 +17,5 @@ MODE_HVSH = 0 # Human vs Human
 MODE_HVSR = 1 # Human vs AIPlayerRandom
 MODE_HVSM = 2 # Human vs AIPlayerMax
 MODE_RVSM = 3 # AIPlayerRandom vs AIPlayerMax
-LIST_MODE = [MODE_HVSH, MODE_HVSR, MODE_HVSM, MODE_RVSM]
-LIST_MODE_DESCR = ["Human vs human", "Human vs AIPlayerRandom", "Human vs AIPlayerMax", "AIPlayerRandom vs AIPlayerMax"]
+LIST_MODE = (MODE_HVSH, MODE_HVSR, MODE_HVSM, MODE_RVSM)
+LIST_MODE_DESCR = ("Human vs human", "Human vs AIPlayerRandom", "Human vs AIPlayerMax", "AIPlayerRandom vs AIPlayerMax")
